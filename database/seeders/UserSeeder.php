996e933b657144c81e18569f682de2e9dc0e6384
Emmanuel Oduro <emmarthurson@gmail.com>
2022-02-16 09:13:07 +0000

    hotfix

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -37,5 +37,26 @@ class UserSeeder extends Seeder
 
       $user->assignRole('super_admin');
 
+      $user = User::create([
+        'name' => 'Emmanuel Oduro',
+        'email' => 'emmanuel@jumeni.com',
+        'password' => Hash::make('decount655'), // password
+        'email_verified_at' => now(),
+        'remember_token' => Str::random(10),
+     ]);
+     Profile::create([
+        'user_id' => 1,
+        'first_name' => 'Emmanuel',
+        'last_name' => 'Oduro',
+        'email' => 'emmanuel@jumeni.com',
+        'phone' => '0241582764',
+        'address' => 'Banana Street 19',
+        'aluminia' => 0,
+        'fb_token' => null,
+     ]);
+
+     $user->assignRole('super_admin');
+
+
    }
 }
