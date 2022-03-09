0d869813869d7052d4eea1b7b459f9beee14fee5
@kwadwomensah <kwadwo.mensah@docuchain.co.uk>
2022-03-09 07:32:01 +0000

    hotfix

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -25,9 +25,7 @@ class EventController extends Controller
       $date = Carbon::parse($request->date)->format('Y-m-d');
       // $events = Event::with(['applications'])->whereMonth('created_at', $date->month)->whereYear('created_at', $date->year)->get();
 
-      var_dump($date);
-
-      $events = Event::with(['applications'])->whereMonth('created_at', '05')->get();
+      $events = Event::whereYear('created_at', $date->year)->get();
 
       // return json_encode(['month' => $date->month, 'year' => $date->year]);
 
