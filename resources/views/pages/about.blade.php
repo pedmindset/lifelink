dc2706007037859bb3a692ea2d0a58769b71ef76
@kwadwomensah <kwadwo.mensah@docuchain.co.uk>
2022-03-22 20:59:23 +0000

    hotfix

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -25,7 +25,7 @@
                <p class="mt-4 text-2xl text-center sm:text-left text-green-400 py-6">Life-Link Friendship Schools</p>
                <p class="mt-4 text-base text-white">Life-Link Friendship Schools Ghana in line with the objectives of Life Link International continues to pioneer efforts to involve the Ghanaian child in the discussion of global issues, as well as providing information and educational materials for the NGOs, the media, and the public at large.
                   Life-Link Ghana also hosts the African Regional Office (ARO) of Life-Link International. It has been actively involved in the work of informing and educating children and the youth in Ghana. It does this by organizing open discussions and school training, symposia, seminars, workshops, projects and conferences such the MUN Conference. It currently has membership of over 3500 in Ghana..</p>
-               <a href="#" class="mt-8 inline-block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100">Shop New Arrivals</a>
+				  {{-- <a href="#" class="mt-8 inline-block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100">Shop New Arrivals</a> --}}
             </div>
          </div>
       </main>
