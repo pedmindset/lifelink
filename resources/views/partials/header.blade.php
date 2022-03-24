9cead117ff56d86fc53fd0809444a985d0d414cd
Emmanuel Oduro <emmarthurson@gmail.com>
2022-03-24 11:30:07 +0000

    hotfix

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
                 <img class="h-8 w-auto sm:h-10" src="{{ asset('img/logo_black.png') }}" alt="">
              </a>
           </div>
-          <div class="-mr-2 -my-2 md:hidden">
+          <div class="-mr-2 -my-2 lg:hidden">
              <button type="button" x-on:click="openMenu = true" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
                 <span class="sr-only">Open menu</span>
                 <!-- Heroicon name: outline/menu -->
