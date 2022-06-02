e5cc3a32347c75cce66c061a86c8790c31269a0a
Emmanuel Oduro <emmarthurson@gmail.com>
2022-06-02 11:27:02 +0000

    hotfix

---
+++ app/Http/Livewire/LoadingScreen.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LoadingScreen extends Component
+{
+    public $loading = false;
+
+    protected $listeners = ['setLoading'];
+
+    public function setLoading(Boolean $boolean)
+    {
+        $this->loading = $boolean
+    }
+    
+    public function render()
+    {
+        return view('livewire.loading-screen');
+    }
+}
