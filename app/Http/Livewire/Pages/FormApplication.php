d979d3a761a4392f69947248eb892038595b70f4
Emmanuel Oduro <emmarthurson@gmail.com>
2022-06-04 20:06:22 +0000

    hotfix

--- app/Http/Livewire/Pages/FormApplication.php
+++ app/Http/Livewire/Pages/FormApplication.php
@@ -22,7 +22,7 @@ class FormApplication extends Component
       $data = EventApplications::firstWhere('id', $this->formId);
       $this->data = $data;
       $this->eventId = $data->event->id;
-      $this->schema = collect(json_decode($this->data->schema, true))->all();
+      $this->schema = collect(json_decode($this->data->schema, true));
       return view('livewire.pages.form-application');
    }
 
