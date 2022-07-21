7b29c744b41a33ef973cecbf7fb57b403dbedfa7
Emmanuel Oduro <emmarthurson@gmail.com>
2022-07-21 08:58:57 +0000

    hotfix

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,6 @@
 @props([
    'type',
 ])
-<button {{ $attributes->merge(['type' => $type, 'class' => 'group relative w-full flex justify-center py-2 px-4 border border-transparent focus:outline-none']) }}>
+<button {{ $attributes->merge(['type' => $type ?? 'button', 'class' => 'group relative w-full flex justify-center py-2 px-4 border border-transparent focus:outline-none']) }}>
    {{ $slot }}
 </button>
